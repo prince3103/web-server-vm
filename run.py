130b1a1e464c3a499f26ed87f156e3b8a350ae24
prince3103 <prince.sethi@trantorinc.com>
2021-03-28 15:34:47 +0530

    changed port of webserver

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ This module imports app instance from app module and runs it.
 from app import app
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port = 8080,debug=True)
+    app.run(host='0.0.0.0', port = 5000,debug=True)
